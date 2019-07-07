5cbcbe7751ab4095596f39520f82de21509c8a8f
nghiadangtrong <nghiadangtrongn92@gmail.com>
2019-07-07 21:44:50 +0700

    Updates

---
+++ precache-manifest.df6cfbc71b38c0368bc79645e3bf39d2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9531d8417ecab6788387deb80195046b",
+    "url": "/robotfriends/index.html"
+  },
+  {
+    "revision": "9a94ae0d2a258f88fb9b",
+    "url": "/robotfriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "d42c633591d0628649b1",
+    "url": "/robotfriends/static/css/main.548868e9.chunk.css"
+  },
+  {
+    "revision": "9a94ae0d2a258f88fb9b",
+    "url": "/robotfriends/static/js/2.c40d8762.chunk.js"
+  },
+  {
+    "revision": "d42c633591d0628649b1",
+    "url": "/robotfriends/static/js/main.6e70dcf7.chunk.js"
+  },
+  {
+    "revision": "aebb4e0e6c2039467182",
+    "url": "/robotfriends/static/js/runtime~main.6fe61ee8.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robotfriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
